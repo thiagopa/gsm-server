3117e46f727e53b1f3bc94439b17fce901ff8c28
Thiago Pagonha Alves <thiago.alves@gsoftware.com.br>
2017-01-18 11:01:17 -0200

    service status

--- app.py
+++ app.py
@@ -3,11 +3,16 @@
     Main Aplication to run service
     @author Thiago Pagonha
 """
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from api.login import login_service
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Service is Running!'
+
 app.register_blueprint(login_service)
 CORS(app)
 
