ad8a5b1f991e468c8fb64926465b26c21b7acdc8
Thiago Pagonha Alves <thiago.alves@gsoftware.com.br>
2017-01-18 15:10:08 -0200

    atualizacao da api

--- api/login.py
+++ api/login.py
@@ -8,16 +8,25 @@ from flask_cors import cross_origin
 
 login_service = Blueprint('login_service', __name__)
 
+users = [{
+    'email' : 'thiago.alves@gsoftware.com.br',
+    'password' : '654321'
+},{
+    'email' : 'admin@gsoftware.com.br',
+    'password' : '123456'
+}]
+
 @login_service.route('/api/login/', methods=['POST'])
 @cross_origin()
 def login():
     content = request.json
-    print(content)
 
     if not content or not content['email'] or not content['password'] :
-        return jsonify({'error': 'Missing request data: email and/or password'}), 400
+        return jsonify({'error': 'Faltam os dados de email e/ou senha'}), 400
 
-    if content['email'] == 'thiago.alves@gsoftware.com.br' and content['password'] == '654321':
+    user = [u for u in users if u['email'].encode('utf-8') == content['email'] and u['password'].encode('utf-8') == content['password']]
+
+    if user:
         return jsonify({'authToken' : '33DE383DFDEE8'}), 200
     else :
         return jsonify({'error': 'Email and/or password incorrect'}), 401
